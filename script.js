4c75bb2319953c2f8df88d5e92fc78a3615ccdeb
raziya8 <raziya.parween.info@gmail.com>
2026-07-13 03:02:53 +0530

    Added invalid city handling with error message

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const apiKey = "4f9e03024209e17101f0ac60afd1b154";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-Icon");
+const weatherIcon = document.querySelector(".weather-icon");
  async function checkWeather(city){
 
     const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
     const data = await response.json();
+    if (response.status === 404)
+   //  !response.ok
+    {
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".weather").style.display = "none";
+   return; 
+   }
+    else
+    {
+      document.querySelector(".error").style.display = "none";
+      document.querySelector(".weather").style.display = "block";
+    }
   
     console.log(data.weather[0].main);
 
@@ -29,9 +41,7 @@ const weatherIcon = document.querySelector(".weather-Icon");
    else if (data.weather[0].main == "Clear"){
        weatherIcon.src ="images 2/clear.png";
     }
-    else if (data.weather[0].main == "Clear"){
-       weatherIcon.src ="images 2/clear.png";
-    }
+  
     else if (data.weather[0].main == "Rain"){
        weatherIcon.src ="images 2/rain.png";
     }
