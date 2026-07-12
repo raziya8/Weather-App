b24131df3ae887391645c0de3c2e0401f7ef3382
raziya8 <raziya.parween.info@gmail.com>
2026-07-13 00:57:57 +0530

    Integrated OpenWeather API and display weather data

---
+++ script.js
@@ -0,0 +1,29 @@
+const apiKey = "4f9e03024209e17101f0ac60afd1b154";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+ async function checkWeather(city) 
+{
+    const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+  
+    console.log(data);
+
+    document.querySelector(".city").innerHTML =data.name;
+   
+    document.querySelector(".temp").innerHTML = 
+    Math.round(data.main.temp) + "°C";
+
+
+    document.querySelector(".humidity").innerHTML =
+    data.main.humidity + "%";
+
+
+    document.querySelector(".wind").innerHTML =
+    data.main.speed + "km/h";
+
+}
+searchBtn.addEventListener("click",() =>{
+    checkWeather(searchBox.value);
+
+});
